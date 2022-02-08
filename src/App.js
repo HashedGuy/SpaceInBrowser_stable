46b01fc812af47891726abc39fa856e4abfa4b54
HashedGuy <hashedguy@gmail.com>
2022-02-08 19:10:05 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom'
+import React, { Suspense, useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import styled from 'styled-components'
+import Earth from './components/Earth'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+export default function App() {
+        return (
+              <CanvasContainer>
+                <Canvas>
+                  <Suspense fallback={null}>
+                    <Earth />
+                  </Suspense>
+                </Canvas>
+              </CanvasContainer>
+        )
 }
-
-export default App;
---
+++ src/components/Earth.js
@@ -0,0 +1,72 @@
+import { useFrame, useLoader } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import React, {useRef} from 'react';
+import * as THREE from 'three'
+
+import { TextureLoader } from 'three';
+import EarthDayMap from "../assets/2k_earth_daymap.jpg";
+import EarthNormalMap from "../assets/2k_earth_normal_map.jpg";
+import EarthSpecularMap from "../assets/2k_earth_specular_map.jpg";
+import EarthCloudsMap from "../assets/2k_earth_clouds.jpg";
+import { PointLight } from 'three';
+
+export function Earth(props) {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  );
+
+  const earthRef = useRef();
+  const cloudsRef = useRef();
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+
+    earthRef.current.rotation.y = elapsedTime / 120;
+    cloudsRef.current.rotation.y = elapsedTime / 120;
+  });
+
+  return (
+    <>
+      {/* <ambientLight intensity={1} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2} />
+      <Stars
+        radius={300}
+        depth={60}
+        count={5000}
+        factor={7}
+        saturation={0}
+        fade={true}
+      />
+      <mesh ref={cloudsRef} position={[0, 0, 3]}>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.4}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+      <mesh ref={earthRef} position={[0, 0, 3]}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
+      </mesh>
+    </>
+  );
+}
+export default Earth;
